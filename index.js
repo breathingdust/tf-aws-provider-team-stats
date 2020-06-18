d7b41970776e659265ce2b7036f43110a6a708ce
Simon Davis <sdavis@hashicorp.com>
2020-06-18 13:41:04 -0700

    add eslint and make changes

--- index.js
+++ index.js
@@ -1,91 +1,95 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-const axios = require('axios')
+const axios = require('axios');
 
-async function main(){
-    const githubToken = core.getInput('github_token');
-    const org = core.getInput('org');
-    const teamSlug = core.getInput('team_slug');
-    const slackToken = core.getInput('slack_token');
-    const slackChannel = core.getInput('slack_channel');
-    const octokit = github.getOctokit(token);   
+async function main() {
+  const githubToken = core.getInput('github_token');
+  const org = core.getInput('org');
+  const teamSlug = core.getInput('team_slug');
+  const slackToken = core.getInput('slack_token');
+  const slackChannel = core.getInput('slack_channel');
+  const octokit = github.getOctokit(githubToken);
 
-    const membersResponse = await octokit.teams.listMembersInOrg({
-        org: org,
-        team_slug: teamSlug
+  const membersResponse = await octokit.teams.listMembersInOrg({
+    org,
+    team_slug: teamSlug,
+  });
+
+  core.info(`Found ${membersResponse.data.length} AWS team members.`);
+
+  const searchQueries = membersResponse.data.map(async (member) => {
+    const response = await octokit.search.issuesAndPullRequests({
+      q: `is:pr is:open author:${member.login} draft:false org:${org}`,
     });
 
-    core.info(`Found ${membersResponse.data.length} AWS team members.`);
-    
-    const searchQueries = membersResponse.data.map(async member => {
-            const response = await octokit.search.issuesAndPullRequests({
-                q : `is:pr is:open author:${member.login} draft:false org:${org}`
-            });
-
-            const result = {
-                member: member.login,
-                count: response.data.total_count,
-            }
-            return result;
-        }
-    );
-
-    const searchResults = await Promise.all(searchQueries);
-
-    searchResults.sort(function(a, b) {
-        var nameA = a.member.toUpperCase();
-        var nameB = b.member.toUpperCase();
-        return (nameA < nameB) ? -1 : (nameA > nameB) ? 1 : 0;
-    });  
-
-    core.setOutput("stats", JSON.stringify(searchResults));
-
-    let memberLines = ""
-
-    searchResults.map(member => memberLines+= `<https://github.com/search?q=org:terraform-providers+author:${member.member}+is:pr+is:open+draft:false|${member.member}> : ${member.count}\n`);
-
-    let postMessageBody = {
-        channel: slackChannel,
-        text: "hi",
-        blocks: [
-            {
-                type: "section",
-                text: {
-                    type: "mrkdwn",
-                    text: `Open Pull Requests for *Organization:* ${org} *Team:* ${teamSlug}`
-                }
-            },
-            {
-                type: "divider"
-            },
-            {
-                type: "section",
-                text: {
-                    type: "mrkdwn",
-                    text: memberLines
-                }
-            }
-        ]
+    return {
+      member: member.login,
+      count: response.data.total_count,
     };
+  });
 
-    axios({
-        method: 'post',
-        url: 'https://slack.com/api/chat.postMessage',
-        headers: {'Authorization': `Bearer ${slackToken}`},
-        data: postMessageBody
-        })
-      .then((res) => {
-        core.info(`Slack Response: ${res.statusCode}`)
-        console.log(res)
-        core.info(res.data);
-      })
-      .catch((error) => {
-        core.error(error)
-      })
+  const searchResults = await Promise.all(searchQueries);
+
+  searchResults.sort((a, b) => {
+    const nameA = a.member.toUpperCase();
+    const nameB = b.member.toUpperCase();
+
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  core.setOutput('stats', JSON.stringify(searchResults));
+
+  let memberLines = '';
+
+  searchResults.foreach((member) => { memberLines += `<https://github.com/search?q=org:terraform-providers+author:${member.member}+is:pr+is:open+draft:false|${member.member}> : ${member.count}\n`; });
+
+  const postMessageBody = {
+    channel: slackChannel,
+    text: 'hi',
+    blocks: [
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: `Open Pull Requests for *Organization:* ${org} *Team:* ${teamSlug}`,
+        },
+      },
+      {
+        type: 'divider',
+      },
+      {
+        type: 'section',
+        text: {
+          type: 'mrkdwn',
+          text: memberLines,
+        },
+      },
+    ],
+  };
+
+  axios({
+    method: 'post',
+    url: 'https://slack.com/api/chat.postMessage',
+    headers: { Authorization: `Bearer ${slackToken}` },
+    data: postMessageBody,
+  })
+    .then((res) => {
+      core.info(`Slack Response: ${res.statusCode}`);
+      core.info(res.data);
+    })
+    .catch((error) => {
+      core.error(error);
+    });
 }
 
-try{
-    main();
+try {
+  main();
 } catch (error) {
-    core.setFailed(error.message);
+  core.setFailed(error.message);
 }
